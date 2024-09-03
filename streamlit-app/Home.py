3ba72ed08bc7a92c49c3a983cf43928d4ccf1cc5
Naimul Islam <90631365+NaimuL0@users.noreply.github.com>
2024-09-03 15:13:56 +0600

    Add files via upload

---
+++ streamlit-app/Home.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="News Summarization Asssignmen", layout="wide")
+
+st.header("News Summarization Asssignment")
+st.subheader("AI based news summarization")
+st.markdown("This app allows you to view news articles and generate summaries.")
